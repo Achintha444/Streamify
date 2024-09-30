03ba695b3a3476f5808f772e573022ccb3f02fb2
Achintha Isuru <achinthaisuru444@gmail.com>
2024-09-29 22:01:55 -0400

    add authentication capability with Asgardeo

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,22 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { AuthProvider, AuthReactConfig } from "@asgardeo/auth-react";
+
+const config: AuthReactConfig = {
+     signInRedirectURL: import.meta.env.VITE_SIGNIN_REDIRECT_URL,
+     signOutRedirectURL: import.meta.env.VITE_SIGNOUT_REDIRECT_URL,
+     clientID: import.meta.env.VITE_CLIENT_ID,
+     baseUrl: import.meta.env.VITE_BASE_URL,
+     scope: (import.meta.env.VITE_SCOPE as string).split(" "),
+};
+
+console.log(config);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AuthProvider config={ config }>
+      <App />
+    </AuthProvider>
   </StrictMode>,
 )
