989eaffbb03aa438a050280c47afb84ad8d78af5
Achintha Isuru <achinthaisuru444@gmail.com>
2024-09-29 22:28:58 -0400

    moved auth configs to `authConfig` in the auth folder

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { AuthProvider, AuthReactConfig } from "@asgardeo/auth-react";
-
-const config: AuthReactConfig = {
-     signInRedirectURL: import.meta.env.VITE_SIGNIN_REDIRECT_URL,
-     signOutRedirectURL: import.meta.env.VITE_SIGNOUT_REDIRECT_URL,
-     clientID: import.meta.env.VITE_CLIENT_ID,
-     baseUrl: import.meta.env.VITE_BASE_URL,
-     scope: (import.meta.env.VITE_SCOPE as string).split(" "),
-};
-
-console.log(config);
+import { AuthProvider } from "@asgardeo/auth-react";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import { authConfig } from './auth/authConfig.ts';
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AuthProvider config={ config }>
+    <AuthProvider config={ authConfig }>
       <App />
     </AuthProvider>
   </StrictMode>,
