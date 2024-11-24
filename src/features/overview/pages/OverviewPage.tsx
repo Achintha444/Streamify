3152559fe77f74ae4f896bebcc2a924f9644ecfd
Achintha Isuru <achinthaisuru444@gmail.com>
2024-11-23 23:26:16 -0500

    drawer list items are styled

--- src/features/overview/pages/OverviewPage.tsx
+++ src/features/overview/pages/OverviewPage.tsx
@@ -1,4 +1,3 @@
-import MuiAppBar, { AppBarProps as MuiAppBarProps } from "@mui/material/AppBar";
 import MuiDrawer from "@mui/material/Drawer";
 import IconButton from "@mui/material/IconButton";
 import List from "@mui/material/List";
@@ -7,10 +6,10 @@ import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import { CSSObject, styled, Theme, useTheme } from "@mui/material/styles";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
 import * as React from "react";
 import Layout from "../../../components/layout/Layout";
+import styles from "../../../components/drawer/Drawer.module.css";
+import { Home01Icon } from "hugeicons-react";
 
 const drawerWidth = 240;
 
@@ -41,33 +40,6 @@ const DrawerHeader = styled("div")(({ theme }) => ({
   ...theme.mixins.toolbar,
 }));
 
-interface AppBarProps extends MuiAppBarProps {
-  open?: boolean;
-}
-
-const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== "open",
-})<AppBarProps>(({ theme }) => ({
-  zIndex: theme.zIndex.drawer + 1,
-  transition: theme.transitions.create(["width", "margin"], {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  variants: [
-    {
-      props: ({ open }) => open,
-      style: {
-        marginLeft: drawerWidth,
-        width: `calc(100% - ${drawerWidth}px)`,
-        transition: theme.transitions.create(["width", "margin"], {
-          easing: theme.transitions.easing.sharp,
-          duration: theme.transitions.duration.enteringScreen,
-        }),
-      },
-    },
-  ],
-}));
-
 const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== "open" })(
   ({ theme }) => ({
     width: drawerWidth,
@@ -93,11 +65,7 @@ const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== "open" 
 
 export default function OverviewPage() {
   const theme = useTheme();
-  const [open, setOpen] = React.useState(false);
-
-  const handleDrawerOpen = () => {
-    setOpen(true);
-  };
+  const [open, setOpen] = React.useState(true);
 
   const handleDrawerClose = () => {
     setOpen(false);
@@ -105,52 +73,21 @@ export default function OverviewPage() {
 
   return (
     <Layout>
-      <AppBar position="fixed" open={open}>
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            onClick={handleDrawerOpen}
-            edge="start"
-            sx={[
-              {
-                marginRight: 5,
-              },
-              open && { display: "none" },
-            ]}
-          >
-            asd
-          </IconButton>
-          <Typography variant="h6" noWrap component="div">
-            Mini variant drawer
-          </Typography>
-        </Toolbar>
-      </AppBar>
       <Drawer open={open}>
         <DrawerHeader>
           <IconButton onClick={handleDrawerClose}>
             {theme.direction === "rtl" ? "asd" : "xx"}
           </IconButton>
         </DrawerHeader>
-        <List>
-          {["Overview", "Users", "Artists", "Songs", "Revenue"].map((text, index) => (
-            <ListItem key={text} disablePadding sx={{ display: "block" }}>
+        <List className={styles["uiDrawerList"]}>
+          {["Overview", "Users", "Artists", "Songs", "Revenue"].map((text) => (
+            <ListItem className={styles["uiDrawerListItem"]} key={text} disablePadding sx={{ display: "block" }}>
               <ListItemButton
-                sx={[
-                  {
-                    minHeight: 48,
-                    px: 2.5,
-                  },
-                  open
-                    ? {
-                      justifyContent: "initial",
-                    }
-                    : {
-                      justifyContent: "center",
-                    },
-                ]}
+                className={styles["uiDrawerListItemButton"]}
               >
+
                 <ListItemIcon
+                  className={styles["uiDrawerListItemIcon"]}
                   sx={[
                     {
                       minWidth: 0,
@@ -165,9 +102,10 @@ export default function OverviewPage() {
                       },
                   ]}
                 >
-                  {index % 2 === 0 ? "asd" : "asd"}
+                  <Home01Icon />
                 </ListItemIcon>
                 <ListItemText
+                  className={styles["uiDrawerListItemText"]}
                   primary={text}
                   sx={[
                     open
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import { AuthProvider } from "@asgardeo/auth-react";
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
-import { authConfig } from './auth/authConfig.ts';
-import { appTheme } from './theme/appTheme.ts';
-import './index.css';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import { authConfig } from "./auth/authConfig.ts";
+import { appTheme } from "./theme/appTheme.ts";
+import "./styles/index.css";
 import { ThemeProvider } from "@mui/material";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
       <AuthProvider config={ authConfig }>
         <ThemeProvider theme={ appTheme }>
