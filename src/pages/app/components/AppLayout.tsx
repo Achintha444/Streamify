7cf6c2fa1663a47160dde00e12401bd33ce27eed
Achintha Isuru <achinthaisuru444@gmail.com>
2024-12-05 00:17:50 -0500

    default routing is set to "overview"

--- src/pages/app/components/AppLayout.tsx
+++ src/pages/app/components/AppLayout.tsx
@@ -1,17 +1,30 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router";
 import UIDrawer from "../../../components/uidrawer/uiDrawer";
 import { MainContentLayout } from "../../../layouts/mainContentLayout";
 import useRouteData from "../../../states/routeData/hooks/useRouteData";
 
 function AppLayout() {
-    const { activeContentRoute } = useRouteData();
+    const { checkIfActiveRoute, activeContentRoute } = useRouteData();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (checkIfActiveRoute("/app") || checkIfActiveRoute("/app/")) {
+            navigate("/app/overview");
+        }
+    }, [ checkIfActiveRoute, navigate ]);
 
     return (
-        <MainContentLayout
-            drawerComponent={ <UIDrawer /> }
-            content={ activeContentRoute!.component }
-            title={ activeContentRoute!.pageTitle }
-            subTitle={ activeContentRoute!.pageSubTitle }
-        />
+        !(checkIfActiveRoute("/app") || checkIfActiveRoute("/app/"))
+            ? (
+                <MainContentLayout
+                    drawerComponent={ <UIDrawer /> }
+                    content={ activeContentRoute!.component }
+                    title={ activeContentRoute!.pageTitle }
+                    subTitle={ activeContentRoute!.pageSubTitle }
+                />
+            )
+            : <div>as</div> // TODO: This need to be replaced with the loading component
     );
 }
 
