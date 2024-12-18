77924d4ce960292de034749b582d869c18e28096
Achintha Isuru <achinthaisuru444@gmail.com>
2024-12-17 23:14:50 -0500

    uiIconMenuList icon design complete

--- src/components/uiIconMenu/uiIconMenu.tsx
+++ src/components/uiIconMenu/uiIconMenu.tsx
@@ -6,6 +6,7 @@ import MenuList from "@mui/material/MenuList";
 import { Logout01Icon } from "hugeicons-react";
 import { FunctionComponent, ReactElement } from "react";
 import styles from "./styles/UIIconMenu.module.css";
+import Typography from "@mui/material/Typography";
 
 interface UIIconMenuListProps {
     iconMenuList: string;
@@ -19,13 +20,31 @@ export const UIIconMenuList: FunctionComponent<UIIconMenuListProps> = (
     } = props;
 
     return (
-        <div className={ styles["uiIconMenuContainer"] }>
-            <MenuList className={ styles["uiIconMenuList"] }>
-                <MenuItem className={ styles["uiIconMenuListItem"] }>
-                    <ListItemIcon>
+        <div className={styles["uiIconMenuContainer"]}>
+            <MenuList className={styles["uiIconMenuList"]}>
+                <MenuItem className={styles["uiIconMenuListItem"]}>
+                    <ListItemIcon className={ styles["uiIconMenuListItemIcon"] }>
                         <Logout01Icon />
                     </ListItemIcon>
-                    <ListItemText>Cut</ListItemText>
+                    <ListItemText
+                        className={styles["uiIconMenuListItemText"]}
+                        primary={(
+                            <Typography variant="body2">
+                                Cut
+                            </Typography>)
+                        } />
+                </MenuItem>
+                <MenuItem className={styles["uiIconMenuListItem"]}>
+                    <ListItemIcon className={ styles["uiIconMenuListItemIcon"] }>
+                        <Logout01Icon />
+                    </ListItemIcon>
+                    <ListItemText
+                        className={styles["uiIconMenuListItemText"]}
+                        primary={(
+                            <Typography variant="body2">
+                                Cut
+                            </Typography>)
+                        } />
                 </MenuItem>
             </MenuList>
         </div>
